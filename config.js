5f364e06a76f17444006530a988275bbaa3b4bdf
Phongphun Kijsanayothin <kphongph@gmail.com>
2016-07-25 06:11:08 +0000

    load with schema

--- config.js
+++ config.js
@@ -1,5 +1,112 @@
 exports.couchdb = {
   url:'http://10.27.65.18:5984',
-  db:'dmc_test'
+  db:'small'
 }
+
+/*
+L5hcQqye69tMkJGifwjraA== รหัสโรงเรียน
+ZyuDe9+wxOw8t4TIGokL8w== ชื่อโรงเรียน
+efPeZGe28XhJ+cIUhqLSBQ== เลขประจำตัวประชาชน
+FZEZ8EDgrK3sM9aUjMikeg== ชั้น
+XaPpI0R6r5+W+pmCzR70FQ== ห้อง
+moqfZUbSh1Yv+b9EoEscSg== รหัสนักเรียน
+AmrB7fxDKndu3eD/JTBxQQ== เพศ
+mXHaTYOmL4k8kjOrVFS+4A== คำนำหน้าชื่อ
+sFZFZLbpd2GLWX/6T/bAFw== ชื่อ
+SdY5d42O0senWB3pVr0UrQ== นามสกุล
+ui7YONiWMpAguuHUECvHtg== ชื่อ (อังกฤษ)
+5FxsOSAwP3S1tMu/zhZ1Mg== นามสกุล (อังกฤษ)
+PfewC115PY7G19xHOoyC6Q== วันเกิด
+FUOwu3S9Z+6kNHym7e7FJQ== อายุ(ปี)
+3BzinAMW032SpUlYl97R2g== อายุ(เดือน)
+X/96XtABxGoz4x9IQXC0lg== หมู่โลหิต
+gnLSL16D05pxoXqMMFkzng== เชื้อชาติ
+wSYlaSOVc3ZETDaL04XWkg== สัญชาติ
+LmgfUW82GXJ/jWer3YAq4Q== ศาสนา
+nt6Xihyo/bIn4XJRryJO3w== จำนวนพี่ชาย
+/u9U4EY6UoTw72xeZrrPaw== จำนวนน้องชาย
+3vx7Db3knqn0nNtcGsbDEA== จำนวนพี่สาว
+utQMEaPf+x5THpzWZMyELQ== จำนวนน้องสาว
+NOOshAIz115rsIh7RX1C7g== เป็นบุตรคนที่
+gMm6gMY9KO2PafgsCfUf8g== สถานภาพสมรสของบิดามารดา
+S2a9ajAS7o3XN+UugR8r0g== หมายเลขบัตรประชาชนบิดา
+iOpvj8WYLZMZfq8McVJnQA== คำนำหน้าชื่อบิดา
+JwclSlOm5Fjryy6W0We0DA== ชื่อบิดา
+XlftldBbl+0ekFkUsPDbWg== นามสกุลบิดา
+n+31ijp3eEfJ6lk0qUc11A== รายได้ต่อเดือนของบิดา
+Y9aWPLCvv07GXIDBqAr9bA== หมายเลขโทรศัพท์ของบิดา
+oHk3d1pPu3ID7GfOPbu33A== หมายเลขบัตรประชาชนมารดา
+1E8WK8ELt7Bb6OINYvhL6w== คำนำหน้าชื่อมารดา
+NxAkuX+FZKgjySojTbDS0w== ชื่อมารดา
+Am2FLaCEkQx7CcIur41ezQ== นามสกุลมารดา
+hU9++9pRE5hokfGGlvTcJQ== รายได้ต่อเดือนของมารดา
+IlGDU77llobBUqBiKk9CJg== หมายเลขโทรศัพท์ของมารดา
+IjVglne0LsuRiKhZr0/gkQ== ความเกี่ยวข้องของผู้ปกครองกับนักเรียน
+urMxnH9RuVmREjpxATdWqA== หมายเลขบัตรประชาชนผู้ปกครอง
+DTLWAVdNok1fCRV5m0FPkw== คำนำหน้าชื่อผู้ปกครอง
+iDLBQOvP/Mvw8CtOnpHICg== ชื่อผู้ปกครอง
+pHeU7lCefDHwrse19VGMJQ== นามสกุลผู้ปกครอง
+ZVPFmlCBPyyHgpYPHMOMAw== รายได้ต่อเดือนของผู้ปกครอง
+ZPMs/TgSLxh2DL1bIaVYiQ== หมายเลขโทรศัพท์ของผู้ปกครอง
+6eR3rpaUg7UcR36maty2Mw== รหัสประจำบ้าน (ทะเบียนบ้าน)
+4f4jjAh0erf7BimmFFxhBA== เลขที่บ้าน (ทะเบียนบ้าน)
+dgNpelkUWbcfE3Lnqlg7NA== หมู่ (ทะเบียนบ้าน)
+5ZsHgSE5Yob5QsuEUlCwbQ== ถนน (ทะเบียนบ้าน)
+r78LEXFzEYNmMHNbIMHoQg== ตำบล (ทะเบียนบ้าน)
+mv8zIkPVeiOvFCDaiTDKoQ== อำเภอ (ทะเบียนบ้าน)
+jXWHawM2toxd6QANIYfpmg== จังหวัด (ทะเบียนบ้าน)
+7K0boy7vNzjeNK3gPIX/bQ== รหัสไปรษณีย์ (ทะเบียนบ้าน)
+6+Z2L6RPxAIu7q4VB3Mkbw== หมายเลขโทรศัพท์ (ทะเบียนบ้าน)
+n5gEFdEDcZyhqz0NtJZAPQ== รหัสประจำบ้าน (ที่อยู่ปัจจุบัน)
+CJnP4hmpST10TiXkIEejHQ== เลขที่บ้าน (ที่อยู่ปัจจุบัน)
+9WCdvd9FMHuIKkPAhfUjuA== หมู่ (ที่อยู่ปัจจุบัน)
+fEWdDy+sRFCsUJdsXru4pQ== ถนน (ที่อยู่ปัจจุบัน)
+tVcqb+4jj40FmUSDmOt1Sg== ตำบล (ที่อยู่ปัจจุบัน)
+9PQBG7sqRx7YiEWyZ2ZdAg== อำเภอ (ที่อยู่ปัจจุบัน)
+YNIVG5yt5Bc1e7uioVSCYA== จังหวัด (ที่อยู่ปัจจุบัน)
++cECUy+fnBdAu+WcjxbsJA== รหัสไปรษณีย์ (ที่อยู่ปัจจุบัน)
+ZHCsnHm1Oweo7CyoUWf5jA== หมายเลขโทรศัพท์ (ที่อยู่ปัจจุบัน)
+LlksQnzVUz6jDtLgkwRyLw== น้ำหนัก
+6cOtFYmI09mU77WW9yWxaw== ส่วนสูง
+l2jsl1O+gBcs5EQwzgj0gg== ความด้อยโอกาส
+Q46uid+nNU63mhzcc1w/Kg== การพักนอนประจำ
+cIe5QPoCbxfdNSYADwRCgA== ขาดแคลนเครื่องแบบ
+OcqLg/TBobAOqoMcT/M3pw== ขาดแคลนเครื่องเขียน
+gS+322ziTzIaXVXslD9oKg== ขาดแคลนแบบเรียน
+rtMYtHxNRSHFvP18436D+g== ขาดแคลนอาหารกลางวัน
+48uYXKOmDVHoRJkokWhW0w== ความพิการ
+Nfx4+aYQymP3qvyaA3vXnA== ระยะทางจากบ้านถึงโรงเรียน (ถนนลูกรัง)
+SQNURR4vawTxAIm8ShNyPA== ระยะทางจากบ้านถึงโรงเรียน (ถนนลาดยาง)
+AQeJ/fSIhlM+Jc4jfBKzPQ== ระยะทางจากบ้านถึงโรงเรียน (ทางน้ำ)
+6jgdU7ihX8I0u4vqLal7YA== ระยะเวลาจากบ้านถึงโรงเรียน
+bWapbgnTnmRbLxF/hG5HIA== ลักษณะการเดินทางมาโรงเรียน
+*/
+
+exports.schema = [
+  {
+    type:'student',
+    fields:[
+      'efPeZGe28XhJ+cIUhqLSBQ==', // cid
+      'AmrB7fxDKndu3eD/JTBxQQ==', // gender
+      'mXHaTYOmL4k8kjOrVFS+4A==', // title
+      'sFZFZLbpd2GLWX/6T/bAFw==', // first name
+      'SdY5d42O0senWB3pVr0UrQ==', // last name
+      'ui7YONiWMpAguuHUECvHtg==', // first name (en)
+      '5FxsOSAwP3S1tMu/zhZ1Mg==', // last name (en)
+      'PfewC115PY7G19xHOoyC6Q=='  // birthdate
+    ]
+  },
+  {
+    type:'dmc',
+    fields:[
+      'efPeZGe28XhJ+cIUhqLSBQ==', // cid
+      'L5hcQqye69tMkJGifwjraA==', // school id
+      'FZEZ8EDgrK3sM9aUjMikeg==', // study grade 
+      'XaPpI0R6r5+W+pmCzR70FQ==', // study room
+      'moqfZUbSh1Yv+b9EoEscSg==',  // student id
+      'record_as'
+    ]
+  },
+]
+
 
--- loader.js
+++ loader.js
@@ -1,14 +1,16 @@
 var fs = require('fs');
+var path = require('path');
 var request = require('request');
 var csv = require('csv-parser');
 var crypto = require('crypto');
 var config = require('./config.js');
+var parseArgs = require('minimist');
 
+// var EventEmitter = require('events').EventEmitter;
+// var loader_event = new EventEmitter();
 // Read CSV content
 
-var csv_name = process.argv[2]?'/'+process.argv[2]:"/test.csv";
-var doc_type = process.argv[3]?process.argv[3]:"dmc";
-
+var options = parseArgs(process.argv.slice(2));
 var buffer = [];
 
 var key_mapping = function(key,idx,dict,cb) {
@@ -58,7 +60,7 @@ var save = function(data,cb) {
 var is_exists = function(md5,cb) {
   var db_url = config.couchdb.url+'/'+ config.couchdb.db;
   var request_url = db_url +
-    '/_design/csv/_view/_row_md5?key="'+md5+'"';
+    '/_design/csv/_view/_csv_md5?key="'+md5+'"';
   // console.log(request_url);
   request(request_url, function(err,response,body) {
     if(!err) {
@@ -110,15 +112,15 @@ var sequence = function(list,index) {
   var index_str = ''+(index+1)+':'+list.length;
   if(index > list.length-1) return;
   var data = list[index];   
-  is_exists(data['row_md5'],function(err,exists) {
+  is_exists(data['csv_md5'],function(err,exists) {
     if(!err) {
       if(exists) {
-        console.log(index_str+' '+'skip     '+data['row_md5']);
+        console.log(index_str+' '+'skip     '+data['csv_md5']);
         sequence(list,++index);
       } else {
         save(data,function(err) {
           if(!err) {
-            console.log(index_str+' '+'inserted '+data['row_md5']);
+            console.log(index_str+' '+'inserted '+data['csv_md5']);
             sequence(list,++index);
           } else {
             console.log(index_str+' '+'error   '+JSON.stringify(err));
@@ -134,28 +136,39 @@ var sequence = function(list,index) {
 };
 
 var map_headers = {};
-
+var re_map_headers = {};
+var working_type = [];
 var count = 0;
 
-fs.createReadStream(__dirname+csv_name).pipe(stream)
+fs.createReadStream(path.join(__dirname,options.csv)).pipe(stream)
 .on('headers',function(headerList) {
   headerList.forEach(function(val) {
     map_headers[val] = thai_hash(val);
+    re_map_headers[map_headers[val]] = val;
+  });
+  config.schema.forEach(function(schema) {
+    var missing = false;
+    schema.fields.forEach(function(field) {
+      if(!re_map_headers[field] && !options[field]) missing = true;
+    });
+    if(!missing) working_type.push(schema);
   });
 })
 .on('data',function(data) {
-   var tmp = {};
-   tmp['type'] = doc_type;
-   for(var key in map_headers) {
-     tmp[map_headers[key]] = data[key];
-   }
-   tmp['row_md5'] = hash_row(tmp);
-   // console.log(count++,tmp['row_md5'],tmp["efPeZGe28XhJ+cIUhqLSBQ=="]);
-   buffer.push(tmp);
+   // console.log(working_type[0]);
+   working_type.forEach(function(schema) {
+     var tmp = {'type':schema.type};
+     schema.fields.forEach(function(field) {
+       tmp[field] = data[re_map_headers[field]]?
+         data[re_map_headers[field]]:options[field];
+     });
+     tmp['csv_md5'] = hash_row(tmp);
+     buffer.push(tmp);
+   });
 })
 .on('end',function() {
    console.log('done reading');
    // console.log(JSON.stringify(map_headers,null,2));
+   // console.log(JSON.stringify(buffer,null,2));
    sequence(buffer,0);
 });
-
